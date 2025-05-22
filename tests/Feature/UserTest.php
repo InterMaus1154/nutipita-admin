6d87fb7a5e54e435a6a05900a02744be3720b108
InterMaus1154 <amarco1154@gmail.com>
2025-05-22 22:53:52 +0100

    Add user test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,12 +10,22 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
     /**
-     * Test unauthenticated user
+     * Test unauthenticated user, should return 401
      */
-    public function test_unauthenticated_user_expect_401(): void
+    public function test_unauthenticated_user_on_dashboard_expect_401(): void
     {
         $response = $this->get('/');
         $response->assertStatus(401);
+    }
 
+    /**
+     * Test authenticated user, should return 200
+     */
+    public function test_authenticated_user_on_dashboard_expect_200()
+    {
+        $user = User::first();
+        $this->actingAs($user);
+        $response = $this->get('/');
+        $response->assertStatus(200);
     }
 }
