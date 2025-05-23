8708bacbad6af53f4af5be8017f6506d78ddaf07
intermaus1154 <amarco1154@gmail.com>
2025-05-23 12:12:40 +0100

    order page show correct product price

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -36,5 +36,68 @@
                 </tbody>
             </table>
         </div>
+        <h2 class="section-title">Order Products</h2>
+        <div class="table-wrapper">
+            <table>
+                <thead>
+                <tr>
+                    <th>
+                        ID
+                    </th>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Order Unit Price
+                    </th>
+                    <th>
+                        Pack Price
+                    </th>
+                    <th>
+                        Unit Weight
+                    </th>
+                    <th>
+                        Qty / pack
+                    </th>
+                    <th>Order Quantity</th>
+                    <th>Total Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($order->products as $product)
+                    <tr>
+                        <td>
+                            <a class="action-link" href="{{route('products.show', compact('product'))}}">
+                                {{$product->product_id}}
+                            </a>
+                        </td>
+                        <td>{{$product->product_name}}</td>
+                        <td>£{{$product->pivot->order_product_unit_price}}</td>
+                        <td>
+                            @if($product->product_qty_per_pack)
+                                £{{$product->pivot->order_product_unit_price * $product->product_qty_per_pack}}
+                            @else
+                                <em>No pack price without qty</em>
+                            @endif
+                        </td>
+                        <td>
+                            @if($product->product_weight_g)
+                                {{$product->product_weight_g}}g
+                            @else
+                                <em>No specified weight</em>
+                            @endif
+                        </td>
+                        <td>
+                            @if($product->product_qty_per_pack)
+                                {{$product->product_qty_per_pack}}pcs
+                            @else
+                                <em>No specified pack quantity</em>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </section>
 </x-layout>
