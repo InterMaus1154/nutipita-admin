a951355e58c7604ab00b792076a05ce0fc4ce50f
InterMaus1154 <amarco1154@gmail.com>
2025-06-06 16:48:45 +0100

    fix row color bug

--- app/Livewire/OrderList.php
+++ app/Livewire/OrderList.php
@@ -59,7 +59,7 @@ class OrderList extends Component
     {
         $this->orders = $this->query
             ->with('customer:customer_id,customer_name', 'products')
-            ->select(['order_status', 'order_placed_at', 'order_due_at', 'customer_id', 'order_id', 'created_at'])
+            ->select(['order_status', 'order_placed_at', 'order_due_at', 'customer_id', 'order_id', 'created_at', 'is_standing'])
             ->orderByDesc('created_at')
             ->get();
     }
