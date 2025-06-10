d507fc85324d8e601efa30624cf3d27bb21341b0
InterMaus1154 <amarco1154@gmail.com>
2025-06-10 14:46:21 +0100

    add login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,33 +8,56 @@
     <link rel="icon" href="{{asset('images/icon_96x96.png?v=3')}}">
     <link rel="apple-touch-icon" href="{{asset('images/icon_96x96.png?v=3')}}" sizes="96x96">
     <meta name="robots" content="noindex">
-    @fluxAppearance
-    @vite(['resources/css/app.css'])
+    @vite('resources/css/app.css')
+    @livewireScripts
     <title>Login</title>
 </head>
 <body>
-<div class="login-page">
-    <div class="form-wrapper">
-        <h2 class="form-title">Login</h2>
-        <x-error/>
-        <form action="{{route('auth.login')}}" method="POST">
-            @csrf
-            {{--input for username--}}
-            <div class="input-wrapper">
-                <label for="username">Username</label>
-                <input type="text" value="{{old('username', '')}}" name="username" id="username"
-                       placeholder="Username">
+<div class="bg-white dark:bg-zinc-800 min-h-screen flex justify-center items-center">
+    <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-2xs dark:bg-neutral-900 dark:border-neutral-700">
+        <div class="p-4 sm:p-7">
+            <div class="text-center">
+                <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
             </div>
-            {{--input for password--}}
-            <div class="input-wrapper">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Password">
+            <div class="mt-5">
+                <!-- Form -->
+                <form method="POST" action="{{route('auth.login')}}">
+                    @csrf
+                    <div class="grid gap-y-4">
+                        <!-- Form Group -->
+                        <div>
+                            <label for="username" class="block text-sm mb-2 dark:text-white">Username</label>
+                            <div class="relative">
+                                <input type="text" id="username" name="username"
+                                       class="py-2.5 sm:py-3 px-4 block w-full border border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                       required aria-describedby="email-error" placeholder="Username">
+                            </div>
+                        </div>
+                        <!-- End Form Group -->
+
+                        <!-- Form Group -->
+                        <div>
+                            <div class="flex flex-wrap justify-between items-center gap-2">
+                                <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
+                            </div>
+                            <div class="relative">
+                                <input type="password" id="password" name="password"
+                                       class="border py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                       required aria-describedby="password-error" placeholder="Password">
+                            </div>
+                        </div>
+                        <!-- End Form Group -->
+                        <flux:button variant="primary" type="submit">Login</flux:button>
+                    </div>
+                </form>
+                <!-- End Form -->
             </div>
-            <input title="Login" type="submit" value="Login" class="form-submit-button">
-        </form>
+        </div>
     </div>
 </div>
+
+
 @fluxScripts
-@livewireScripts
+
 </body>
 </html>
