18c1a3eaf136faf5992d99531a662d1b33b7194b
intermaus1154 <amarco1154@gmail.com>
2025-06-12 12:12:51 +0100

    customise error component

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,12 +1,18 @@
 @if($errors->any())
     <div class="space-y-4 my-4">
         @foreach($errors->all() as $error)
-            <p class="text-red-400 text-lg">{{$error}}</p>
+            <div class="flex gap-1 items-center">
+                <flux:icon.exclamation-triangle class="text-red-400 text-lg"/>
+                <p class="text-red-400 text-lg">{{$error}}</p>
+            </div>
         @endforeach
     </div>
 @endif
 @if(session()->has('error'))
     <div class="space-y-4 my-4">
-        <p class="text-red-400 text-lg">{{session()->get('error')}}</p>
+        <div class="flex gap-1 items-center">
+            <flux:icon.exclamation-triangle class="text-red-400 text-lg"/>
+            <p class="text-red-400 text-lg">{{$error}}</p>
+        </div>
     </div>
 @endif
