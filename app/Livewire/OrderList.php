a6ac13d00b1d07118abe1737e62238ecfd701b07
InterMaus1154 <amarco1154@gmail.com>
2025-06-13 20:06:41 +0100

    add pagination

--- app/Livewire/OrderList.php
+++ app/Livewire/OrderList.php
@@ -6,44 +6,52 @@ use App\Models\Customer;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Route;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class OrderList extends Component
 {
-    public Collection $orders;
-    private Builder $query;
+
+    use WithPagination;
+
+    public $filters = [
+        'customer_id' => null,
+        'due_from' => null,
+        'due_to' => null,
+        'status' => null,
+    ];
 
     public bool $isOnIndex = true;
 
     public function mount()
     {
-        $this->query = Order::query();
-        $this->loadOrders();
-
         $this->isOnIndex = Route::is('orders.index');
     }
 
-    public function delete(int $order_id)
-    {
-        $order = Order::find($order_id);
-        if ($order) {
-            $order->products()->detach();
-            $order->delete();
-        }
-
-        $this->query = Order::query();
-        $this->loadOrders();
-    }
+//    public function delete(int $order_id)
+//    {
+//        $order = Order::find($order_id);
+//        if ($order) {
+//            $order->products()->detach();
+//            $order->delete();
+//        }
+//
+//        $this->query = Order::query();
+//        $this->loadOrders();
+//    }
 
     #[On('update-filter')]
     public function applyFilter(array $filters)
     {
-        $this->query = Order::query();
-
-        $this->query
+        $this->resetPage();
+        $this->filters = $filters;
+    }
+    public function render()
+    {
+        $filters = $this->filters;
+        $query = Order::query()
             ->when(!empty($filters['customer_id']), function ($builder) use ($filters) {
                 return $builder->where('customer_id', $filters['customer_id']);
             })
@@ -57,23 +65,14 @@ class OrderList extends Component
                 return $builder->where('order_status', $filters['status']);
             });
 
-        $this->loadOrders();
-    }
-
-    public function loadOrders()
-    {
-        $this->orders = $this->query
-            ->with('customer:customer_id,customer_name', 'products')
+        $orders = $query->with('customer:customer_id,customer_name', 'products')
             ->orderByDesc('order_placed_at')
-            ->get();
-    }
+            ->paginate(2);
 
-    public function render()
-    {
         $products = Product::select(['product_id', 'product_name'])->get();
         return view('livewire.order-list', [
             'products' => $products,
-            'orders' => $this->orders,
+            'orders' => $orders,
             'onOrderIndex' => $this->isOnIndex
         ]);
     }
--- resources/views/livewire/order-list.blade.php
+++ resources/views/livewire/order-list.blade.php
@@ -1,6 +1,6 @@
 @use(Illuminate\Support\Carbon)
 @use(Illuminate\Support\Facades\Request)
-<div>
+<div class="space-y-4">
     <div class="flex flex-col">
         <div class="-m-1.5 overflow-x-auto">
             <div class="p-1.5 min-w-full inline-block align-middle">
@@ -116,5 +116,8 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div>
+        {{$orders->links('vendor.pagination.tailwind')}}
     </div>
 </div>
