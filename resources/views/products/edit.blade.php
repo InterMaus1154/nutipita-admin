8490ef3caa48d11a8da8a5143e42194f8b93e0f5
intermaus1154 <amarco1154@gmail.com>
2025-06-15 00:06:14 +0100

    update product page

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,32 +1,35 @@
-<x-layout>
-    <div class="form-wrapper">
-        <h2 class="form-title">Update product</h2>
-        <x-error />
-        <form action="{{route('products.update', compact('product'))}}" method="POST">
+<x-flux-layout>
+    <x-page-section>
+        <x-page-heading title="Edit '{{$product->product_name}}'"/>
+        <x-error/>
+        <x-success/>
+        <form action="{{route('products.update', compact('product'))}}" method="POST" class="flex flex-col gap-4">
             @csrf
             @method('PUT')
             {{--Product name--}}
-            <div class="input-wrapper">
-                <label for="product_name">Product Name</label>
-                <input type="text" id="product_name" name="product_name" value="{{$product->product_name}}" />
-            </div>
-            {{--Product unit price--}}
-            <div class="input-wrapper">
-                <label for="product_unit_price">Product unit price</label>
-                <input type="number" id="product_unit_price" name="product_unit_price" value="{{$product->product_unit_price}}" step=".01"/>
+            <div class="max-w-sm">
+                <label for="product_name" class="block text-sm font-medium mb-2 dark:text-white">Product Name</label>
+                <input type="text" id="product_name" name="product_name" value="{{$product->product_name}}"
+                       class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"/>
             </div>
             {{--Product weight--}}
-            <div class="input-wrapper">
-                <label for="product_weight_g">Product unit weight (gramm) (optional)</label>
-                <input type="number" id="product_weight_g" name="product_weight_g" value="{{$product->product_weight_g}}" />
+            <div class="max-w-sm">
+                <label for="product_weight_g" class="block text-sm font-medium mb-2 dark:text-white">Product unit weight
+                    (gramm) (optional)</label>
+                <input type="number" id="product_weight_g" name="product_weight_g"
+                       value="{{$product->product_weight_g}}"
+                       class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"/>
             </div>
             {{--Product qty per pack--}}
-            <div class="input-wrapper">
-                <label for="product_qty_per_pack">Product quantity per pack (optional)</label>
-                <input type="number" id="product_qty_per_pack" name="product_qty_per_pack" value="{{$product->product_qty_per_pack}}" />
+            <div class="max-w-sm">
+                <label for="product_qty_per_pack" class="block text-sm font-medium mb-2 dark:text-white">Product
+                    quantity per pack (optional)</label>
+                <input type="number" id="product_qty_per_pack" name="product_qty_per_pack"
+                       value="{{$product->product_qty_per_pack}}"
+                       class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"/>
             </div>
-            <input type="submit" value="Update" class="form-submit-button">
+            <flux:button variant="primary" type="submit">Update</flux:button>
         </form>
-    </div>
-</x-layout>
+    </x-page-section>
+</x-flux-layout>
 <?php
