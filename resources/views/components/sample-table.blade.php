c251e4c4b914d882be1912fe8b1712ffc314767c
InterMaus1154 <amarco1154@gmail.com>
2025-06-14 16:20:45 +0100

    new customers page

---
+++ resources/views/components/sample-table.blade.php
@@ -0,0 +1,20 @@
+<div class="flex flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+        <div class="p-1.5 min-w-full inline-block align-middle">
+            <div class="overflow-hidden">
+                <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                    <thead>
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="odd:bg-white even:bg-gray-100 hover:bg-gray-100 dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700 text-center">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,92 +1,107 @@
 <x-flux-layout>
     <x-page-section>
         <x-page-heading title="Customers"/>
-        <x-error />
+        <x-error/>
         <x-success/>
-        <a href="{{route('customers.create')}}" class="action-link">Add new customer</a>
-        <div class="table-wrapper">
-            <table>
-                <thead>
-                <tr>
-                    <th>
-                        ID
-                    </th>
-                    <th>
-                        Name
-                    </th>
-                    <th>
-                        Address
-                    </th>
-                    <th>
-                        Email
-                    </th>
-                    <th>
-                        Phone
-                    </th>
-                    @foreach($products as $product)
-                        <th>{{$product->product_name}}</th>
-                    @endforeach
-                    <th>
-                        Total Orders
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                @forelse($customers as $customer)
-                    <tr>
-                        <td>
-                            <a class="action-link" href="{{route('customers.show', compact('customer'))}}">
-                                {{$customer->customer_id}}
-                            </a>
-                        </td>
-                        <td>
-                            <a href="{{route('customers.show', compact('customer'))}}" class="action-link">
-                                {{$customer->customer_name}}
-                            </a>
-                        </td>
-                        <td>
-                            {{$customer->short_address}}
-                        </td>
-                        <td>
-                            @if($customer->customer_email)
-                                <a class="action-link"
-                                   href="mailto:{{$customer->customer_email}}">{{$customer->customer_email}}</a>
-                            @else
-                                <em>No email provided</em>
-                            @endif
-                        </td>
-                        <td>
-                            @if($customer->customer_phone)
-                                <a class="action-link"
-                                   href="tel:{{$customer->customer_phone}}">{{$customer->customer_phone}}</a>
-                            @else
-                                <em>No phone provided</em>
-                            @endif
-                        </td>
-                        @foreach($products as $product)
-                            @php
-                                $product = $product->setCurrentCustomer($customer);
-                            @endphp
-                            <td>
-                                {{$product->price === 0 ? "" : "£".$product->price}}
-                            </td>
-                        @endforeach
-                        <td>
-                            {{$customer->orders_count}}</td>
-                        <td>
-                            <a class="action-link table" href="{{route('customers.edit', compact('customer'))}}">Edit
-                                Details</a>
-                            <a class="action-link table" href="{{route('customers.edit.custom-price', compact('customer'))}}">Edit
-                                prices</a>
-                        </td>
-                    </tr>
-                @empty
-                    <tr style="text-align: center">
-                        <td>No customers found!</td>
-                    </tr>
-                @endforelse
-                </tbody>
-            </table>
+        <flux:link href="{{route('customers.create')}}">Add new customer</flux:link>
+        <div class="flex flex-col">
+            <div class="-m-1.5 overflow-x-auto">
+                <div class="p-1.5 min-w-full inline-block align-middle">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                            <thead>
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                    ID
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                    Name
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                    Address
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                    Email
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                    Phone
+                                </th>
+                                @foreach($products as $product)
+                                    <th scope="col"
+                                        class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">{{$product->product_name}}</th>
+                                @endforeach
+                                <th scope="col"
+                                    class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                    Total Orders
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">
+                                    Actions
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($customers as $customer)
+                                <tr class="odd:bg-white even:bg-gray-100 hover:bg-gray-100 dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700 text-center">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        <flux:link
+                                            href="{{route('customers.index', compact('customer'))}}">{{$customer->customer_id}}</flux:link>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        <flux:link
+                                            href="{{route('customers.index', compact('customer'))}}">{{$customer->customer_name}}</flux:link>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        {{$customer->short_address}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        @if($customer->customer_email)
+                                            <flux:link
+                                                href="mailto:{{$customer->customer_email}}">{{$customer->customer_email}}</flux:link>
+                                        @else
+                                            <em>No email provided!</em>
+                                        @endif
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        @if($customer->customer_phone)
+                                            <flux:link
+                                                href="tel:{{$customer->customer_phone}}">{{$customer->customer_phone}}</flux:link>
+                                        @else
+                                            <em>No phone provided!</em>
+                                        @endif
+                                    </td>
+                                    @foreach($products as $product)
+                                        @php
+                                            $product = $product->setCurrentCustomer($customer);
+                                        @endphp
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                            {{$product->price === 0 ? "" : "£".$product->price}}
+                                        </td>
+                                    @endforeach
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        {{$customer->orders_count}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200 space-x-1.5">
+                                        <flux:link href="{{route('customers.show', compact('customer'))}}">View
+                                        </flux:link>
+                                        <flux:link href="{{route('customers.edit', compact('customer'))}}">Edit
+                                        </flux:link>
+                                        <flux:link href="{{route('customers.edit.custom-price', compact('customer'))}}">
+                                            Edit Prices
+                                        </flux:link>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </x-page-section>
 </x-flux-layout>
