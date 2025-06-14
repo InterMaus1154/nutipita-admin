56264ca69910c29b133513b46e95ef06bb59487e
InterMaus1154 <amarco1154@gmail.com>
2025-06-14 16:32:03 +0100

    standing order page

--- resources/views/standing_orders/index.blade.php
+++ resources/views/standing_orders/index.blade.php
@@ -1,57 +1,53 @@
-<x-layout>
-    <section class="page-section">
-        <h2 class="section-title">Standing Orders</h2>
+<x-flux-layout>
+    <x-page-section >
+        <x-page-heading title="Standing Orders"/>
         <x-success/>
-        <a class="action-link" href="{{route('standing-orders.create')}}">Add new standing order</a>
-        <div class="table-wrapper">
-            <table>
-                <thead>
-                <tr>
-                    <th>
-                        ID #
-                    </th>
-                    <th>
-                        Customer
-                    </th>
-                    <th>
-                        Start From
-                    </th>
-                    <th>
-                        Status
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                @forelse($orders as $order)
-                    <tr>
-                        <td>
-                            <a class="action-link" href="{{route('standing-orders.show', compact('order'))}}">
-                                #{{$order->standing_order_id}}
-                            </a>
-                        </td>
-                        <td>
-                            <a href="{{route('customers.show', ['customer' => $order->customer])}}" class="action-link">
-                                {{$order->customer->customer_name}}
-                            </a>
-                        </td>
-                        <td>
-                            {{dayDate($order->start_from)}}
-                        </td>
-                        <td>
-                            {{$order->is_active ? "Active" : "Inactive"}}
-                        </td>
-                        <td>
-                            <a href="{{route('standing-orders.edit', compact('order'))}}" class="action-link">Edit</a>
-                        </td>
-                    </tr>
-                @empty
-                    <tr style="text-align: center">
-                        <td>No orders found!</td>
-                    </tr>
-                @endforelse
-                </tbody>
-            </table>
+        <x-error />
+        <flux:link href="{{route('standing-orders.create')}}">Add new standing order</flux:link>
+        <div class="flex flex-col">
+            <div class="-m-1.5 overflow-x-auto">
+                <div class="p-1.5 min-w-full inline-block align-middle">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                            <thead>
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">#ID</th>
+                                <th scope="col" class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">Customer</th>
+                                <th scope="col" class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">Start From</th>
+                                <th scope="col" class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">Status</th>
+                                <th scope="col" class="px-6 py-3 text-center text-md font-medium text-gray-500 uppercase dark:text-neutral-500">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @forelse($orders as $order)
+                                <tr class="odd:bg-white even:bg-gray-100 hover:bg-gray-100 dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700 text-center">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        <flux:link href="{{route('standing-orders.show', compact('order'))}}">#{{$order->standing_order_id}}</flux:link>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        <flux:link href="{{route('customers.show', ['customer' => $order->customer])}}">{{$order->customer->customer_name}}</flux:link>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                        {{daydate($order->start_from)}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200 italic">
+                                        {{$order->is_active ? "Active" : "Inactive"}}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200 space-x-1.5">
+                                        <flux:link href="{{route('standing-orders.show', compact('order'))}}">View</flux:link>
+                                        <flux:link href="{{route('standing-orders.edit', compact('order'))}}">Edit</flux:link>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr class="odd:bg-white even:bg-gray-100 hover:bg-gray-100 dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700 text-center">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">No orders found!</td>
+                                </tr>
+                            @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-
-    </section>
-</x-layout>
+    </x-page-section>
+</x-flux-layout>
