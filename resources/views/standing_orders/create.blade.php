1bae74f80bec32148265fcfb9be01ecccede65a2
intermaus1154 <amarco1154@gmail.com>
2025-06-15 14:23:15 +0100

    standing order create page

--- resources/views/standing_orders/create.blade.php
+++ resources/views/standing_orders/create.blade.php
@@ -1,44 +1,48 @@
-<x-layout>
-    <div class="form-wrapper standing-order-form">
-        <h2 class="form-title">Add new standing order</h2>
+<x-flux-layout>
+    <x-page-section>
+        <x-page-heading title="Add new standing order"/>
+        <x-success/>
         <x-error/>
-        <form action="{{route('standing-orders.store')}}" method="POST">
+        <form action="{{route('standing-orders.store')}}" method="POST" class="flex flex-col gap-4">
             @csrf
-            <div class="input-wrapper">
-                <label for="customer_id">Customer</label>
-                <select name="customer_id" id="customer_id">
+            <div class="max-w-sm">
+                <label for="customer_id" class="block text-sm font-medium mb-2 dark:text-white">Customer</label>
+                <select name="customer_id" id="customer_id" class="py-3 px-4 pe-9 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600">
                     <option value="">---Select customer---</option>
                     @foreach($customers as $customer)
                         <option value="{{$customer->customer_id}}">{{$customer->customer_name}}</option>
                     @endforeach
                 </select>
             </div>
-            <div class="input-wrapper">
-                <label for="start_from">Start From</label>
+            <div class="max-w-sm">
+                <label for="start_from" class="block text-sm font-medium mb-2 dark:text-white">Start From</label>
                 <input type="date" id="start_from" name="start_from"
-                       value="{{old('start_from', now()->toDateString())}}">
+                       value="{{old('start_from', now()->toDateString())}}"
+                       class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
             </div>
-            <div class="days-wrapper">
+            <div class="flex gap-8 flex-wrap my-2">
                 {{--count the days from 0 to 7--}}
                 @for($i = 0; $i<7;$i++)
-                    <div class="day-wrapper">
-                        <h4>{{\Illuminate\Support\Carbon::create()->startOfWeek()->addDays($i)->dayName}}</h4>
+                    <div class="flex flex-col gap-4 bg-white border border-gray-200 shadow-2xs rounded-xl p-4 md:p-5 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400">
+                        <h4 class="font-bold">{{\Illuminate\Support\Carbon::create()->startOfWeek()->addDays($i)->dayName}}</h4>
                         @foreach($products as $product)
-                            <div class="input-wrapper">
+                            <div class="max-w-sm">
                                 <label
-                                    for="products[{{$i}}][{{$product->product_id}}]">{{$product->product_name}}</label>
+                                    for="products[{{$i}}][{{$product->product_id}}]"
+                                    class="block text-sm font-medium mb-2 dark:text-white">{{$product->product_name}}</label>
                                 <input
                                     type="number"
                                     value="0"
                                     id="products[{{$i}}][{{$product->product_id}}]"
                                     name="products[{{$i}}][{{$product->product_id}}]"
+                                    class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                                 >
                             </div>
                         @endforeach
                     </div>
                 @endfor
             </div>
-            <input type="submit" class="form-submit-button" value="Add">
+            <flux:button variant="primary" type="submit">Add</flux:button>
         </form>
-    </div>
-</x-layout>
+    </x-page-section>
+</x-flux-layout>
