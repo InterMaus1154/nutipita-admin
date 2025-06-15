1ad60963a65cd443746aa620e828dd7f4476cc72
intermaus1154 <amarco1154@gmail.com>
2025-06-15 12:18:59 +0100

    custom prices page

--- resources/views/customers/edit-custom-price.blade.php
+++ resources/views/customers/edit-custom-price.blade.php
@@ -1,25 +1,29 @@
-<x-layout>
-    <div class="form-wrapper">
-        <h2 class="form-title">Prices for {{$customer->customer_name}}</h2>
-        <h3>0 or empty will be deleted</h3>
+<x-flux-layout>
+    <x-page-section>
+        <x-page-heading title="Prices for {{$customer->customer_name}}"/>
+        <flux:badge icon="exclamation-circle">0 or empty will be deleted</flux:badge>
         <x-error/>
-        <form action="{{route('customers.update.custom-price', compact('customer'))}}" method="POST">
+        <x-success/>
+        <form action="{{route('customers.update.custom-price', compact('customer'))}}" method="POST"
+              class="flex flex-col gap-4">
             @csrf
             @method('PUT')
             @foreach($products as $product)
-                <div class="input-wrapper">
-                    <label for="products[{{$product->product_id}}]">{{$product->product_name}}</label>
+                <div class="max-w-sm">
+                    <label for="products[{{$product->product_id}}]"
+                           class="block text-sm font-medium mb-2 dark:text-white">{{$product->product_name}}</label>
                     <input type="number"
                            id="products[{{$product->product_id}}]"
                            name="products[{{$product->product_id}}]"
                            value="{{$product->price}}"
                            step="0.01"
                            placeholder="Unit price"
+                           class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                     >
                 </div>
             @endforeach
-            <input type="submit" value="Submit" class="form-submit-button">
+            <flux:button variant="primary" type="submit">Submit</flux:button>
         </form>
-    </div>
-</x-layout>
+    </x-page-section>
+</x-flux-layout>
 
