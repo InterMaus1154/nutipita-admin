40f5fa80d3a54de8ad32e7d3906636348e8ccb72
InterMaus1154 <amarco1154@gmail.com>
2025-07-31 14:39:19 +0100

    prepare day time orders on homepage

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,21 +3,36 @@
     <x-page-section>
         @php($week = now()->weekOfYear())
         <x-page-heading title="Today Orders - Week {{$week}}"/>
-        <flux:link href="{{route('orders.create', ['order_placed_at' => now()->toDateString(), 'order_due_at' => now()->addDay()->toDateString()])}}">Place
-            New Order for Today
-        </flux:link>
         @if($orders->isEmpty())
             <em>No due orders for today!</em>
         @else
-            <flux:link href="{{route('today.order.pdf')}}">Download total order PDF</flux:link>
-{{--            <div class="flex gap-6 flex-wrap">--}}
-{{--                <x-data-box dataBoxHeader="Daily Income" :dataBoxValue="moneyFormat($totalDayIncome)"/>--}}
-{{--                <x-data-box dataBoxHeader="Total Pita" :dataBoxValue="amountFormat($totalDayPita)"/>--}}
-{{--                @foreach($productTotals as $key => $value)--}}
-{{--                    <x-data-box :dataBoxHeader="$key" :dataBoxValue="amountFormat($value)"/>--}}
-{{--                @endforeach--}}
-{{--            </div>--}}
-            <livewire:order-list :withSummaryData="true" :summaryVisibleByDefault="true" :filters="['due_from'=>$dueDate, 'due_to' => $dueDate]"/>
+            <div class="flex flex-col gap-8">
+                {{--day time orders--}}
+                <div class="flex flex-col gap-4">
+                    <div class="flex gap-4">
+                        <flux:icon.sun />
+                        <h3 class="font-bold">Day Time Orders</h3>
+                    </div>
+                    <flux:link>
+                        Place New Order for Day Time
+                    </flux:link>
+                    <flux:link>Download total day orders PDF</flux:link>
+                    <flux:badge color="orange">Coming soon...</flux:badge>
+                </div>
+                <flux:separator />
+                {{--night time orders--}}
+                <div class="flex flex-col gap-4">
+                    <div class="flex gap-4">
+                        <flux:icon.moon />
+                        <h3 class="font-bold">Night Time Orders</h3>
+                    </div>
+                    <flux:link href="{{route('orders.create', ['order_placed_at' => now()->toDateString(), 'order_due_at' => now()->addDay()->toDateString()])}}">Place
+                        New Order for Tonight
+                    </flux:link>
+                    <flux:link href="{{route('today.order.pdf')}}">Download total night orders PDF</flux:link>
+                    <livewire:order-list :withSummaryData="true" :summaryVisibleByDefault="true" :filters="['due_from'=>$dueDate, 'due_to' => $dueDate]"/>
+                </div>
+            </div>
         @endif
     </x-page-section>
 </x-flux-layout>
