fbf3840b858391c0a94e4e8dfb963ef0fb66dca9
InterMaus1154 <amarco1154@gmail.com>
2025-07-31 12:26:21 +0100

    add model resolver as helper global

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,8 +1,9 @@
 <?php
 // custom helpers
 
+use App\Helpers\ModelResolver;
 use Carbon\Carbon;
-use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
 use App\Helpers\Format;
 
 if (!function_exists('dayDate')) {
@@ -43,53 +44,6 @@ if (!function_exists('roundUp')) {
         return Format::roundUp($amount, $precision);
     }
 }
-
-//if (!function_exists('formatMoney')) {
-//    /**
-//     * Format money for 2 or 3 decimal places
-//     * If 3 by default, leave it as it is
-//     * Never shows more than 2 significant decimal places
-//     * For example
-//     * 0.2550 -> 0.255
-//     * 0.2500 -> 0.25
-//     * 0.2 -> 0.20
-//     * 500 -> 500.00
-//     * @param mixed $amount
-//     * @return string
-//     */
-//    function formatMoney(mixed $amount): string
-//    {
-//        // convert to float
-//        try {
-//            $amount = (float)$amount;
-//        } catch (Exception $e) {
-//            return "ERROR IN MONEY FORMATTING, CANNOT CONVERT TO FLOAT!";
-//        }
-//
-//        // enforce 3 decimal digits, and proper number format
-//        $formatted = number_format($amount, 3, '.', ',');
-//
-//        // remove trailing zeros (from the right), then remove the decimal separator
-//        $formatted = rtrim(rtrim($formatted, '0'), '.');
-//
-//        // if decimal separator is found (eg 0.255)
-//        if (str_contains($formatted, '.')) {
-//            // find decimal separator
-//            $decimalSeparatorPos = strpos($formatted, '.');
-//
-//            // get the decimal part
-//            $decimals = substr($formatted, $decimalSeparatorPos + 1);
-//            // if the num of decimals are less than 2, add a trailing zero to make it 2 decimals, eg 0.2 -> 0.20
-//            if (strlen($decimals) < 2) {
-//                $formatted = $formatted . '0';
-//            }
-//        } else {
-//            // if no decimal separator, add trailing zero digits for proper money format
-//            $formatted = $formatted . '.00';
-//        }
-//        return $formatted;
-//    }
-
 
 if (!function_exists('numberFormat')) {
     /**
@@ -136,3 +90,11 @@ if (!function_exists('unitPriceFormat')) {
         return '£'.Format::unitPriceFormat($amount);
     }
 }
+
+if(!function_exists('resolveModel')){
+    function resolveModel(Model|int|string $id, string $modelClass): Model|null
+    {
+        return ModelResolver::resolve($id, $modelClass);
+    }
+}
+
