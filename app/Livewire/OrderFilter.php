da84e6ea062cf6adc9cbbc171b732b6e7accf261
InterMaus1154 <amarco1154@gmail.com>
2025-08-04 14:02:42 +0100

    remove quick filter methods from orderfilter - trait is used

--- app/Livewire/OrderFilter.php
+++ app/Livewire/OrderFilter.php
@@ -3,15 +3,18 @@
 namespace App\Livewire;
 
 use App\Models\Customer;
+use App\Traits\HasQuickDueFilter;
 use Carbon\Carbon;
 use Livewire\Component;
 
 class OrderFilter extends Component
 {
 
+    use HasQuickDueFilter;
+
+    protected bool $dispatchAble = true;
+
     public int|string|null $customer_id = null;
-    public ?string $due_from = null;
-    public ?string $due_to = null;
     public ?string $status = null;
     public ?int $month = null;
     public bool $cancelled_order_hidden = true;
@@ -63,42 +66,6 @@ class OrderFilter extends Component
     public function clearFilter()
     {
         $this->reset();
-        $this->dispatchEvent();
-    }
-
-    public function setToday(): void
-    {
-        $this->due_from = now()->addDay()->toDateString();
-        $this->due_to = now()->addDay()->toDateString();
-
-        $this->dispatchEvent();
-    }
-
-    public function setWeek(): void
-    {
-        $this->due_from = now()->startOfWeek()->subDay()->format('Y-m-d');
-        $this->due_to = now()->endOfWeek()->subDay()->format('Y-m-d');
-        $this->dispatchEvent();
-    }
-
-    public function setMonth(): void
-    {
-        $this->due_from = now()->startOfMonth()->toDateString();
-        $this->due_to = now()->endOfMonth()->toDateString();
-        $this->dispatchEvent();
-    }
-
-    public function setYear()
-    {
-        $this->due_from = now()->startOfYear()->toDateString();
-        $this->due_to = now()->endOfYear()->toDateString();
-        $this->dispatchEvent();
-    }
-
-    public function setYesterday()
-    {
-        $this->due_from = now()->toDateString();
-        $this->due_to = now()->toDateString();
         $this->dispatchEvent();
     }
 
