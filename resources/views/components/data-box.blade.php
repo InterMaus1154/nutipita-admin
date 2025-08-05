1ba2f3020a8226d415f571cf1394ae0cbfa16671
InterMaus1154 <amarco1154@gmail.com>
2025-08-05 14:44:09 +0100

    change style of summary boxes

--- resources/views/components/data-box.blade.php
+++ resources/views/components/data-box.blade.php
@@ -1,6 +1,6 @@
 @props(['dataBoxValue', 'dataBoxHeader'])
 <div
-    class="flex flex-col gap-2 bg-white border border-gray-200 shadow-2xs rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:text-white text-center items-center text-xl">
+    class="flex flex-col justify-between gap-2 bg-white border border-gray-200 shadow-2xs rounded-xl p-5 text-lg sm:text-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-white text-center items-center ">
     <span>{{$dataBoxHeader}}</span>
     <span>{{$dataBoxValue}}</span>
 </div>
--- resources/views/livewire/order-summary.blade.php
+++ resources/views/livewire/order-summary.blade.php
@@ -3,15 +3,17 @@
         <div class="space-y-4">
             <flux:button wire:click="toggleSummaries()">Toggle Summaries</flux:button>
             <div @class([
-                        'hidden' => !$visible
+                        'hidden' => !$visible,
+                        'flex flex-col gap-4'
                     ])>
-                <div class="flex gap-6 flex-wrap">
+                <div class="grid grid-cols-3 gap-4 sm:flex sm:gap-6 flex-wrap">
                     @if($withIncome)
                         <x-data-box dataBoxHeader="Total Income" :dataBoxValue="moneyFormat($totalIncome)"/>
                     @endif
                     <x-data-box dataBoxHeader="Total Orders" :dataBoxValue="amountFormat(collect($orders)->count())"/>
                     <x-data-box dataBoxHeader="Total Pita" :dataBoxValue="amountFormat($totalPita)"/>
-
+                </div>
+                <div class="grid grid-cols-2 gap-4 sm:flex sm:gap-6 flex-wrap">
                     @foreach($productTotals as $productTotal)
                         @unless(empty($productTotal['qty']))
                             <x-data-box wire:key="$productTotal['name']"
