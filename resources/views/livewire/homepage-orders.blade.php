c10799aa5dbd10c67be38ee7ab33231299fd4050
InterMaus1154 <amarco1154@gmail.com>
2025-08-11 14:22:06 +0100

    remove junk

--- resources/views/livewire/homepage-orders.blade.php
+++ resources/views/livewire/homepage-orders.blade.php
@@ -6,16 +6,7 @@
                 <flux:icon.sun/>
                 <h3 class="font-bold text-xl sm:text-2xl">Day Time Orders</h3>
             </div>
-            <flux:link
-                href="{{ route('orders.create', [
-                            'order_placed_at' => now()->toDateString(),
-                            'order_due_at' => now()->toDateString(),
-                            'is_daytime' => 1
-                        ]) }}">
-                Place New Order for Day Time
-            </flux:link>
             <div class="flex flex-col gap-4" id="dayTimeOrdersContainer">
-                <flux:link>Download total day orders PDF</flux:link>
                 <livewire:order-list
                     wire:key="day-orders"
                     :withSummaryData="true"
@@ -41,17 +32,7 @@
                 <flux:icon.moon/>
                 <h3 class="font-bold text-xl sm:text-2xl">Night Time Orders</h3>
             </div>
-            <flux:link
-                href="{{ route('orders.create', [
-                            'order_placed_at' => now()->toDateString(),
-                            'order_due_at' => now()->addDay()->toDateString()
-                        ]) }}">
-                Place New Order for Tonight
-            </flux:link>
             <div class="flex flex-col gap-4" id="nightTimeOrdersContainer">
-                <flux:link href="{{ route('today.order.pdf') }}">
-                    Download total night orders PDF
-                </flux:link>
                 <livewire:order-list
                     wire:key="night-orders"
                     :withSummaryData="true"
