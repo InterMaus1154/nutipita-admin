126a200b5ba7f047fe94fc2953640880b9b27b86
InterMaus1154 <amarco1154@gmail.com>
2025-08-18 13:48:16 +0100

    export mobile select sort to a component

--- resources/views/livewire/order-list.blade.php
+++ resources/views/livewire/order-list.blade.php
@@ -14,23 +14,7 @@
     <x-error/>
     {{--sorting for mobile--}}
     @if($orders->isNotEmpty() && $withMobileSort)
-        <div class="sm:hidden">
-            <x-form.form-label id="mobile_sort" text="Sort by"/>
-            <x-form.form-select id="mobile_sort" wireModelLive="mobileSort">
-                <option value="desc:customer">Customer Name (desc)</option>
-                <option value="asc:customer">Customer Name (asc)</option>
-                <option value="desc:order_placed_at">Placed At (desc)</option>
-                <option value="asc:order_placed_at">Placed At (asc)</option>
-                <option value="desc:order_due_at">Due At (desc)</option>
-                <option value="asc:order_due_at">Due At (asc)</option>
-                <option value="desc:order_status">Status (desc)</option>
-                <option value="asc:order_status">Status (asc)</option>
-                <option value="desc:total_pita">Total Pita (desc)</option>
-                <option value="asc:total_pita">Total Pita (asc)</option>
-                <option value="desc:total_price">Total Price (desc)</option>
-                <option value="asc:total_price">Total Price (asc)</option>
-            </x-form.form-select>
-        </div>
+        <x-order.mobile-order-select-sort />
     @endif
     {{--order summary--}}
     @if($withSummaryData)
