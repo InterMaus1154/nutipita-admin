f9b237c6739a7bea5c3e1fa61d3806a5bb5b7968
InterMaus1154 <amarco1154@gmail.com>
2025-08-18 22:51:30 +0100

    remove unwanted order list on page if order count is < 0

--- app/Livewire/Homepage/HomepageOrders.php
+++ app/Livewire/Homepage/HomepageOrders.php
@@ -2,20 +2,53 @@
 
 namespace App\Livewire\Homepage;
 
+use App\Models\Order;
 use Illuminate\View\View;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class HomepageOrders extends Component
 {
-    public bool $is_daytime = true;
-    public bool $is_nighttime = true;
+    public bool $isDaytime = true;
+    public bool $isNighttime = true;
+
+    public array $dayTimeFilters = [];
+    public array $nightTimeFilters = [];
+
+    public bool $hasDayTimeOrders;
+    public bool $hasNightTimeOrders;
+
+    public function mount(): void
+    {
+        $this->dayTimeFilters = [
+            'due_from'=>now()->toDateString(),
+            'due_to' => now()->toDateString(),
+            'daytime_only' => true
+        ];
+
+        $this->nightTimeFilters = [
+            'due_from' => now()->addDay()->toDateString(),
+            'due_to' => now()->addDay()->toDateString(),
+            'nighttime_only' => true
+        ];
+
+        $this->hasDayTimeOrders = Order::query()
+            ->whereDate('order_due_at', '=', $this->dayTimeFilters['due_from'])
+            ->where('is_daytime', true)
+            ->exists();
+
+        $this->hasNightTimeOrders = Order::query()
+            ->whereDate('order_due_at', '=', $this->nightTimeFilters['due_from'])
+            ->where('is_daytime', false)
+            ->exists();
+
+    }
 
     #[On('toggle-homepage')]
     public function applyFilter(array $values): void
     {
-        $this->is_daytime = $values['is_daytime'];
-        $this->is_nighttime = $values['is_nighttime'];
+        $this->isDaytime = $values['is_daytime'];
+        $this->isNighttime = $values['is_nighttime'];
     }
 
     public function render(): View
--- resources/views/livewire/homepage-orders.blade.php
+++ resources/views/livewire/homepage-orders.blade.php
@@ -2,12 +2,8 @@
     <livewire:homepage.homepage-toggler />
     <div class="flex flex-col gap-20">
         {{-- Day Time Orders --}}
-        @if($is_daytime)
-            <div class="flex flex-col gap-4">
-                {{--            <div class="flex gap-4 items-center justify-center">--}}
-                {{--                <flux:icon.sun/>--}}
-                {{--                <h3 class="font-bold text-xl sm:text-2xl">Day Time Orders</h3>--}}
-                {{--            </div>--}}
+        @if($isDaytime && $hasDayTimeOrders)
+            <div class="flex flex-col gap-4" id="dayTimeOrders">
                 <div class="flex flex-col gap-4" id="dayTimeOrdersContainer">
                     <livewire:order-list
                         wire:key="day-orders"
@@ -15,26 +11,18 @@
                         :withIncome="false"
                         :with-mobile-sort="true"
                         :summaryVisibleByDefault="true"
-                        :prop-filters="[
-                                'due_from'=>now()->toDateString(),
-                                'due_to' => now()->toDateString(),
-                                'daytime_only' => true
-                            ]"
+                        :prop-filters="$dayTimeFilters"
                     />
                 </div>
             </div>
         @endif
-        @if($is_daytime && $is_nighttime)
+        @if($isDaytime && $isNighttime && $hasNightTimeOrders && $hasDayTimeOrders)
             {{--TODO--}}
             <div class="h-[1px] border-b-1 border-b-neutral-600 w-[75%] mx-auto justify-self-center"></div>
         @endif
         {{-- Night Time Orders --}}
-        @if($is_nighttime)
-            <div class="flex flex-col gap-4">
-                {{--            <div class="flex gap-4 items-center justify-center">--}}
-                {{--                <flux:icon.moon/>--}}
-                {{--                <h3 class="font-bold text-xl sm:text-2xl">Night Time Orders</h3>--}}
-                {{--            </div>--}}
+        @if($isNighttime && $hasNightTimeOrders)
+            <div class="flex flex-col gap-4" id="nightTimeOrders">
                 <div class="flex flex-col gap-4" id="nightTimeOrdersContainer">
                     <livewire:order-list
                         wire:key="night-orders"
@@ -42,11 +30,7 @@
                         :withIncome="false"
                         :with-mobile-sort="true"
                         :summaryVisibleByDefault="true"
-                        :prop-filters="[
-                            'due_from' => now()->addDay()->toDateString(),
-                            'due_to' => now()->addDay()->toDateString(),
-                            'nighttime_only' => true
-                        ]"
+                        :prop-filters="$nightTimeFilters"
                     />
                 </div>
             </div>
