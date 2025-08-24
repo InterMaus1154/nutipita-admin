10b2f21483f9e27e1165b75cee463058fdef71de
InterMaus1154 <amarco1154@gmail.com>
2025-08-24 18:47:20 +0100

    add ui popup component

---
+++ resources/views/components/ui/detail-popup-card.blade.php
@@ -0,0 +1,23 @@
+<div class="flex flex-col gap-4">
+    {{--extra info section--}}
+    {{--backdrop--}}
+    <div class="fixed inset-0 min-h-screen z-101 bg-black/60" x-show="detailsMenuOpen"
+         x-transition></div>
+    <div
+        class="flex-col gap-4 flex fixed bottom-0 z-102 overflow-y-scroll min-h-[60vh] left-0 right-0 dark:bg-zinc-800/80 backdrop-blur-lg rounded-t-xl p-4 border-t-6 border-t-accent"
+        x-cloak x-show="detailsMenuOpen" x-on:click.outside="detailsMenuOpen = false"
+        x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 translate-y-full"
+        x-transition:enter-end="opacity-100 translate-y-0"
+        x-transition:leave="transition ease-in duration-300"
+        x-transition:leave-start="opacity-100 translate-y-0"
+        x-transition:leave-end="opacity-0 translate-y-full">
+        <div x-on:click="detailsMenuOpen = false" class="absolute right-2 top-2">
+            <flux:icon.x-circle class="text-accent size-8"/>
+        </div>
+        {{$slot}}
+    </div>
+    <flux:button x-on:click="detailsMenuOpen = !detailsMenuOpen">
+        <flux:icon.chevron-double-up class="text-accent"/>
+    </flux:button>
+</div>
