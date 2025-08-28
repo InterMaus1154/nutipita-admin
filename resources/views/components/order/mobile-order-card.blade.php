7ebac2b40ccf610657a47bfa62c9ac4104d340c8
InterMaus1154 <amarco1154@gmail.com>
2025-08-28 10:54:46 +0100

    small design change for order card

--- resources/views/components/order/mobile-order-card.blade.php
+++ resources/views/components/order/mobile-order-card.blade.php
@@ -109,7 +109,7 @@
                     <flux:separator/>
                 @endif
                 <div class="text-base flex gap-4 justify-between items-center">
-                    <span>{{$orderProduct->product_name}}</span>
+                    <span>{{$orderProduct->product_name}} {{$orderProduct->product_weight_g}}g</span>
                     <div class="flex flex-col gap-1 justify-center items-center text-center">
                         <span>@amountFormat($orderProduct->pivot->product_qty) x @unitPriceFormat($orderProduct->pivot->order_product_unit_price)</span>
                         <span>@moneyFormat($orderProduct->pivot->product_qty * $orderProduct->pivot->order_product_unit_price)</span>
@@ -120,11 +120,13 @@
         </div>
         {{--        total pita and price info--}}
         <div class="flex justify-between gap-4 items-center flex-wrap">
-            <div class="flex gap-2">
-                <span class="text-lg text-accent font-semibold">@amountFormat($order->total_pita)</span>
+            <div class="flex gap-2 items-center">
+                <flux:icon.pita class="size-5 text-accent"/>
+                <span class="text-lg font-semibold">@amountFormat($order->total_pita)</span>
             </div>
-            <div class="flex gap-2">
-                <span class="text-lg text-accent font-semibold">@moneyFormat($order->total_price)</span>
+            <div class="flex gap-2 items-center">
+                <flux:icon.circle-pound-sterling class="size-5 text-accent"/>
+                <span class="text-lg font-semibold">@moneyFormat($order->total_price)</span>
             </div>
         </div>
         {{--action buttons--}}
