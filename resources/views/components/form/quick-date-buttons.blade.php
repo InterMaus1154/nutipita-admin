0db04931a1ef7a9fc777a1cafc23a8c049ec9c01
InterMaus1154 <amarco1154@gmail.com>
2025-08-30 16:34:26 +0100

    fix: quick date buttons week wanst auto selected on saturday

--- resources/views/components/form/quick-date-buttons.blade.php
+++ resources/views/components/form/quick-date-buttons.blade.php
@@ -69,9 +69,9 @@
             @foreach(getWeeksOfTheYear() as $index => $week)
                 @php
                     $today = now()->toDateString();
-                    $isCurrentWeek = $today >= $week['start_date'] && $today < $week['end_date'];
+                    $isCurrentWeek = $today >= $week['start_date'] && $today <= $week['end_date'];
                 @endphp
-                <div class="cursor-pointer text-center py-1 px-4 rounded-sm hover:bg-accent/90 transition-all hover:text-black"
+                <div class="cursor-pointer text-center py-1 px-4 rounded-sm hover:bg-neutral-500/50"
                      x-on:click="selectedWeek = {{$index}}; weekOpen = false"
                      :class="selectedWeek === {{$index}} || (selectedWeek === null) && {{$isCurrentWeek ? 'true' : 'false'}} ? 'border-2 border-accent' : 'border-2 border-transparent' "
                      wire:click="setWeek('{{$week['start_date']}}', '{{$week['end_date']}}')"
@@ -89,7 +89,7 @@
                     $today = now()->toDateString();
                     $isCurrentMonth = $today >= $month['start_date'] && $today < $month['end_date'];
                 @endphp
-                <div class="cursor-pointer text-center py-1 rounded-sm hover:bg-accent/90 transition-all hover:text-black"
+                <div class="cursor-pointer text-center py-1 rounded-sm hover:bg-neutral-500/50"
                      x-on:click="selectedMonth = {{$index}}; monthOpen = false"
                      :class="selectedMonth === {{$index}} || (selectedMonth === null) && {{$isCurrentMonth ? 'true' : 'false'}} ? 'border-2 border-accent' : 'border-2 border-transparent' "
                      wire:click="setMonth('{{$month['start_date']}}', '{{$month['end_date']}}')"
@@ -101,4 +101,3 @@
         </div>
     </div>
 </div>
-
