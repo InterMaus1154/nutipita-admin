bed6f92bbe4cb540858be051a44a19f945e08675
InterMaus1154 <amarco1154@gmail.com>
2025-09-05 17:52:28 +0100

    csv check in file import

--- app/Livewire/FinRecord/FileImport.php
+++ app/Livewire/FinRecord/FileImport.php
@@ -2,11 +2,28 @@
 
 namespace App\Livewire\FinRecord;
 
+use Illuminate\View\View;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use function Laravel\Prompts\alert;
 
 class FileImport extends Component
 {
-    public function render()
+    use WithFileUploads;
+
+    public $file;
+
+    public function updatedFile(): void
+    {
+        $extension = $this->file->getClientOriginalExtension();
+        if($extension !== "csv"){
+            session()->flash('error', 'File needs to be .csv only!');
+            $this->redirectRoute('financial-records.create');
+        }
+
+    }
+
+    public function render(): View
     {
         return view('livewire.fin-record.file-import');
     }
--- resources/views/livewire/fin-record/file-import.blade.php
+++ resources/views/livewire/fin-record/file-import.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-auto mt-10">
     <div class="relative">
         <label for="file" class="border-1 border-zinc-600 bg-neutral-900 rounded-md py-3 px-4 cursor-pointer text-neutral-500 hover:text-accent font-semibold">Import Data</label>
-        <input type="file" id="file" class="absolute opacity-0"/>
+        <input type="file" id="file" class="absolute opacity-0" wire:model="file" accept=".csv"/>
     </div>
 </div>
