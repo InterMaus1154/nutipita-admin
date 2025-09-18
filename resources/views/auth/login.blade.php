ba0097a78fab3033d51711eae8ccbbd9651d1179
InterMaus1154 <amarco1154@gmail.com>
2025-09-18 10:28:58 +0100

    change style of custom select

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,56 +14,40 @@
 </head>
 <body class="bg-white dark:bg-zinc-800">
 <div class="min-h-screen flex justify-center items-center">
-    <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-2xs dark:bg-neutral-900 dark:border-neutral-700">
-        <div class="p-4 sm:p-7">
+    <div class="bg-white border border-gray-200 rounded-xl dark:bg-neutral-900 dark:border-neutral-700 shadow-xl">
+        <div class="p-4 sm:p-7 flex flex-col gap-2">
             <div class="text-center">
-                <h1 class="block text-2xl font-bold text-gray-800 dark:text-white ">Sign in</h1>
+                <h1 class="block text-2xl font-bold text-gray-800 dark:text-white ">Login</h1>
             </div>
             <div class="space-y-4">
                 <x-error/>
             </div>
-            <div class="mt-5">
-                <!-- Form -->
+            <div>
                 <form method="POST" action="{{route('auth.login')}}">
                     @csrf
-                    <div class="grid gap-y-4">
-                        <!-- Form Group -->
-                        <div>
-                            <label for="username" class="block text-sm mb-2 dark:text-white">Username</label>
-                            <div class="relative">
-                                <input type="text" id="username" name="username"
-                                       class="py-2.5 sm:py-3 px-4 block w-full border border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                                       required aria-describedby="email-error" placeholder="Username">
-                            </div>
-                        </div>
-                        <!-- End Form Group -->
-                        <!-- Form Group -->
-                        <div>
-                            <div class="flex flex-wrap justify-between items-center gap-2">
-                                <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
-                            </div>
-                            <div class="relative">
-                                <input type="password" id="password" name="password"
-                                       class="border py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                                       required aria-describedby="password-error" placeholder="Password">
-                            </div>
-                        </div>
-                        <!-- End Form Group -->
+                    <div class="grid gap-6">
+                        <x-form.form-wrapper>
+                            <x-form.form-label id="username" text="Username"/>
+                            <x-form.form-input id="username" type="text" name="username" placeholder="Username"/>
+                        </x-form.form-wrapper>
+                        <x-form.form-wrapper>
+                            <x-form.form-label id="password" text="Password"/>
+                            <x-form.form-input id="password" type="password" name="password" placeholder="Password"/>
+                        </x-form.form-wrapper>
                         <flux:button variant="primary" type="submit">Login</flux:button>
                     </div>
                 </form>
-                <!-- End Form -->
             </div>
         </div>
     </div>
     <flux:button class="!fixed bottom-0 right-0" id="infoBtn">
-        <flux:icon.information-circle />
+        <flux:icon.information-circle/>
     </flux:button>
 </div>
 <script>
     const infoBtn = document.querySelector("#infoBtn");
-    infoBtn.addEventListener("click", ()=>{
-       alert("Szerbusz!");
+    infoBtn.addEventListener("click", () => {
+        alert("Szerbusz!");
     });
 </script>
 @fluxScripts
--- resources/views/components/ui/select/option.blade.php
+++ resources/views/components/ui/select/option.blade.php
@@ -7,6 +7,6 @@
 <li
     data-value="{{$value}}"
     x-on:click="open = false; selected = @js($value);"
-    class="cursor-pointer rounded-2xl py-2 text-center font-bold hover:scale-105 sm:mx-2 transition-all duration-300 outline-2 outline-transparent sm:hover:outline-accent sm:hover:bg-black/20">
+    class="cursor-pointer rounded-2xl py-2 text-center font-medium hover:scale-110 sm:mx-2 transition-all duration-300 outline-2 outline-transparent sm:hover:outline-accent sm:hover:bg-black/20">
     {{ \Illuminate\Support\Str::limit($text, 18) }}
 </li>
