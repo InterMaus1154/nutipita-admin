e54943b13906307dcea2315eee1be12f2b24d3cd
intermaus1154 <amarco1154@gmail.com>
2025-12-08 15:04:59 +0000

    fix standing order activate bug

--- app/Console/Commands/CheckForStandingOrdersToBeActivated.php
+++ app/Console/Commands/CheckForStandingOrdersToBeActivated.php
@@ -35,8 +35,7 @@ class CheckForStandingOrdersToBeActivated extends Command
             $orders = StandingOrder::query()
                 ->where('is_active', false)
                 ->whereDate('start_from', '=', today())
-                ->whereNull('is_forced' )
-                ->select(['is_active'])
+                ->whereNull('is_forced')
                 ->get();
             foreach ($orders as $order) {
                 $order->update([
