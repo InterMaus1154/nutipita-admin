ab0a9d2d006c4a0ccad20f6c4986bd707809ed4b
intermaus1154 <amarco1154@gmail.com>
2025-12-28 10:28:41 +0000

    fix filtering on financial records

--- resources/views/livewire/fin-record/financial-record-toggler.blade.php
+++ resources/views/livewire/fin-record/financial-record-toggler.blade.php
@@ -1,26 +1,38 @@
 @use(App\Enums\FinancialRecordType)
-<div>
+<div x-data="{selectedType: @entangle('selectedType')}">
     {{--show both--}}
-    @if(is_null($selectedType))
-        <div class="flex flex-col sm:grid grid-cols-2 gap-4">
-            <div class="flex flex-col gap-4">
-                <h2 class="text-center">Income</h2>
-                <livewire:fin-record.financial-record-list :type="\App\Enums\FinancialRecordType::INCOME"/>
-            </div>
-            <div class="flex flex-col gap-4">
-                <h2 class="text-center">Expense</h2>
-                <livewire:fin-record.financial-record-list :type="\App\Enums\FinancialRecordType::EXPENSE"/>
-            </div>
-        </div>
-    @elseif($selectedType === FinancialRecordType::EXPENSE)
-        <div class="flex flex-col gap-4">
-            <h2 class="text-center">Expense</h2>
-            <livewire:fin-record.financial-record-list :type="\App\Enums\FinancialRecordType::EXPENSE"/>
-        </div>
-    @elseif($selectedType === FinancialRecordType::INCOME)
-        <div class="flex flex-col gap-4">
+    <div
+        class="flex flex-col sm:grid gap-4"
+        :class="selectedType === null ? 'grid-cols-2' : 'grid-cols-1'">
+        <div
+            x-show="selectedType === null || selectedType === @js(FinancialRecordType::INCOME->value)"
+            x-cloak
+            class="flex flex-col gap-4">
             <h2 class="text-center">Income</h2>
-            <livewire:fin-record.financial-record-list :type="\App\Enums\FinancialRecordType::INCOME"/>
+            <livewire:fin-record.financial-record-list :type="FinancialRecordType::INCOME"/>
         </div>
-    @endif
+        <div
+            x-show="selectedType === null || selectedType === @js(FinancialRecordType::EXPENSE->value)"
+            x-cloak
+            class="flex flex-col gap-4">
+            <h2 class="text-center">Expense</h2>
+            <livewire:fin-record.financial-record-list :type="FinancialRecordType::EXPENSE"/>
+        </div>
+    </div>
+{{--    --}}{{--expense only--}}
+{{--    <div--}}
+{{--        x-show="selectedType === '@js(FinancialRecordType::EXPENSE->value)'"--}}
+{{--        x-cloak--}}
+{{--        class="flex flex-col gap-4">--}}
+{{--        <h2 class="text-center">Expense</h2>--}}
+{{--        <livewire:fin-record.financial-record-list :type="FinancialRecordType::EXPENSE"/>--}}
+{{--    </div>--}}
+{{--    --}}{{--income only--}}
+{{--    <div--}}
+{{--        x-show="selectedType === '@js(FinancialRecordType::INCOME->value)'"--}}
+{{--        x-cloak--}}
+{{--        class="flex flex-col gap-4">--}}
+{{--        <h2 class="text-center">Income</h2>--}}
+{{--        <livewire:fin-record.financial-record-list :type="FinancialRecordType::INCOME"/>--}}
+{{--    </div>--}}
 </div>
