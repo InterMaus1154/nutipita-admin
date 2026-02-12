9ba81c83f50816570eec4df21cce022e43cee0e8
intermaus1154 <amarco1154@gmail.com>
2026-02-12 10:49:04 +0000

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,16 +16,29 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         if (!app()->isProduction()) {
-            User::create([
-                'username' => 'test1',
-                'password' => 'test'
-            ]);
+
 
             $file = __DIR__ . '/nutipita_prod_db.sql';
             if (!file_exists($file)) return; // file may not be present, as not pushed to git
             try {
+
+                DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+                $tables = DB::select('SHOW TABLES');
+                $dbName = config('database.connections.mysql.database');
+
+                foreach ($tables as $table) {
+                    $tableName = $table->{"Tables_in_$dbName"};
+                    DB::statement("DROP TABLE IF EXISTS `$tableName`");
+                }
+
                 $content = file_get_contents($file);
                 DB::unprepared($content);
+
+                User::create([
+                    'username' => 'test1',
+                    'password' => 'test'
+                ]);
             } catch (\Exception $e) {
                 echo $e->getMessage();
             }
