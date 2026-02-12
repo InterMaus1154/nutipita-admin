1fab80a8d603e35ff6ef378a86f1d9b9b5170eb7
intermaus1154 <amarco1154@gmail.com>
2026-02-12 11:14:10 +0000

    add debug text to header

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -18,7 +18,8 @@
         <flux:navlist.item icon="file-text" href="{{route('invoices.index')}}" :current="Route::is('invoices.*')">
             Invoices
         </flux:navlist.item>
-        <flux:navlist.item icon="currency-pound" href="{{route('financial-records.index')}}" :current="request()->is('financial-records*')">Money
+        <flux:navlist.item icon="currency-pound" href="{{route('financial-records.index')}}"
+                           :current="request()->is('financial-records*')">Money
             Management
         </flux:navlist.item>
         <flux:navlist.item icon="arrow-path-rounded-square" href="{{route('standing-orders.index')}}"
@@ -30,10 +31,11 @@
         <flux:navlist.item icon="pita" href="{{route('products.index')}}" :current="Route::is('products.*')">
             Products
         </flux:navlist.item>
-        <flux:navlist.item :current="Route::is('settings.*')" icon="cog-6-tooth" x-data="{menuOpen: false}" x-on:click="menuOpen= true" class="relative cursor-pointer">
+        <flux:navlist.item :current="Route::is('settings.*')" icon="cog-6-tooth" x-data="{menuOpen: false}"
+                           x-on:click="menuOpen= true" class="relative cursor-pointer">
             Settings
             <flux:icon.chevron-right class="size-5 text-accent transition-transform duration-300"
-                x-bind:class="{'rotate-90': menuOpen}"
+                                     x-bind:class="{'rotate-90': menuOpen}"
             />
             <div x-show="menuOpen"
                  x-transition
@@ -42,7 +44,8 @@
                  class="absolute z-100 left-[-50%] translate-x-[50%] top-10 border-2 border-neutral-700 rounded-xl bg-zinc-800/60 backdrop-blur-lg p-2 flex flex-col gap-4 w-full action">
                 <ul>
                     <li>
-                        <x-ui.mobile-card-dropdown-link href="{{route('settings.appearance')}}" class="flex! gap-2 py-2">
+                        <x-ui.mobile-card-dropdown-link href="{{route('settings.appearance')}}"
+                                                        class="flex! gap-2 py-2">
                             <flux:icon.monitor class="text-black dark:text-white size-4"/>
                             Appearance
                         </x-ui.mobile-card-dropdown-link>
@@ -67,12 +70,15 @@
     </flux:dropdown>
 </flux:sidebar>
 
-<flux:header class="grid grid-cols-[1fr_1fr] sm:grid-cols-[1fr_auto_1fr] items-center z-100 bg-zinc-800 shadow-md py-2" sticky>
+<flux:header class="grid grid-cols-[1fr_1fr] sm:grid-cols-[1fr_auto_1fr] items-center z-100 bg-zinc-800 shadow-md py-2"
+             sticky>
     <flux:sidebar.toggle icon="bars-2" inset="left" class="sm:hidden"/>
     <div class="justify-self-start! max-sm:justify-self-end! flex gap-2 items-center sm:flex-row-reverse">
         <div>
             <span class="font-bold">
-            Week {{getCurrentWeekNumber()}}
+                Week {{getCurrentWeekNumber()}} @if(!app()->isProduction()) <span class="text-red-500">DEBUG</span>
+
+                @endif
             </span>
         </div>
         <flux:dropdown position="top" align="right">
@@ -95,7 +101,8 @@
                                :current="Route::is('orders.*')"></flux:navlist.item>
             <flux:navlist.item icon="file-text" href="{{route('invoices.index')}}" title="Invoices"
                                :current="Route::is('invoices.*')"></flux:navlist.item>
-            <flux:navlist.item icon="currency-pound" href="{{route('financial-records.index')}}" title="Money Management"
+            <flux:navlist.item icon="currency-pound" href="{{route('financial-records.index')}}"
+                               title="Money Management"
                                :current="request()->is('financial-records*')"></flux:navlist.item>
             <flux:navlist.item icon="arrow-path-rounded-square" href="{{route('standing-orders.index')}}"
                                title="Standing orders" :current="Route::is('standing-orders.*')"></flux:navlist.item>
@@ -103,7 +110,8 @@
                                :current="Route::is('customers.*')"></flux:navlist.item>
             <flux:navlist.item icon="pita" href="{{route('products.index')}}" title="Products"
                                :current="Route::is('products.*')"></flux:navlist.item>
-            <flux:navlist.item :current="Route::is('settings.*')" icon="cog-6-tooth" x-data="{menuOpen: false}" x-on:click="menuOpen = true" class="cursor-pointer relative">
+            <flux:navlist.item :current="Route::is('settings.*')" icon="cog-6-tooth" x-data="{menuOpen: false}"
+                               x-on:click="menuOpen = true" class="cursor-pointer relative">
                 <div x-show="menuOpen"
                      x-transition
                      x-on:click.outside="menuOpen = false"
