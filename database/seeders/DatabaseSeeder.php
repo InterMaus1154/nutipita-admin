665201fbe070c989d3585c2806fb0bac574b7eb7
intermaus1154 <amarco1154@gmail.com>
2026-02-12 10:34:13 +0000

    add prod db and gitignore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,34 +11,6 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    private function createData()
-    {
-        Product::create([
-            'product_name' => 'White normal',
-            'product_weight_g' => 100,
-            'product_qty_per_pack' => 5
-        ]);
-
-        Product::create([
-            'product_name' => 'White medium',
-            'product_weight_g' => 60,
-            'product_qty_per_pack' => 15
-        ]);
-
-        Product::create([
-            'product_name' => 'Brown normal',
-            'product_weight_g' => 100,
-            'product_qty_per_pack' => 5
-        ]);
-
-        Product::create([
-            'product_name' => 'Brown medium',
-            'product_weight_g' => 60,
-            'product_qty_per_pack' => 15
-        ]);
-
-//        Customer::factory(2)->create();
-    }
 
     /**
      * Seed the application's database.
