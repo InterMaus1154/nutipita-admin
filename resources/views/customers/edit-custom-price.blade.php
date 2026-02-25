24ce333822878ba95088034bc1921c2c9e840022
Mark <amarco1154@gmail.com>
2026-02-25 09:04:36 +0000

    include old value in creating custom price

--- resources/views/customers/edit-custom-price.blade.php
+++ resources/views/customers/edit-custom-price.blade.php
@@ -17,11 +17,12 @@
                         <flux:badge>
                             {{$product->product_weight_g}}g
                         </flux:badge>
-                        </label>
+                    </label>
                     <input type="number"
                            id="products[{{$product->product_id}}]"
                            name="products[{{$product->product_id}}]"
                            placeholder="Unit price"
+                           value="{{old('products.'.$product->product_id, '')}}"
                            step="0.0001"
                            class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                     >
