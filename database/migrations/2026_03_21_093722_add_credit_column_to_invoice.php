5b4802755098e2d7483b431f0a5b30d1ed10ba61
Mark <amarco1154@gmail.com>
2026-03-21 09:37:50 +0000

    add migration

---
+++ database/migrations/2026_03_21_093722_add_credit_column_to_invoice.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->decimal('invoice_credit', 10, 4)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropColumn('invoice_credit');
+        });
+    }
+};
