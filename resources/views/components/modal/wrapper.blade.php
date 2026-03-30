581264ee1edadf6f37c99c603e6ce470de948a5f
intermaus1154 <amarco1154@gmail.com>
2026-03-30 18:27:03 +0200

    change title font size on modal

--- resources/views/components/modal/wrapper.blade.php
+++ resources/views/components/modal/wrapper.blade.php
@@ -31,7 +31,7 @@
 <div class="w-full {{$sizes[$size]}} bg-zinc-800 rounded-md divide-y divide-zinc-500 mx-auto">
 
     <header class="relative {{$sectionPadding}}">
-        <h1 class="text-4xl text-center font-bold text-accent">{{$title}}</h1>
+        <h1 class="text-3xl text-center font-bold text-accent">{{$title}}</h1>
         <flux:button @click="$dispatch('modal-close')" variant="primary" class="absolute! right-2 top-2">X</flux:button>
     </header>
 
