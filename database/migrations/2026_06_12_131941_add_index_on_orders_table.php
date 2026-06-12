17cfc80558cd7b5e465a354861385a17c4afb6c5
intermaus1154 <amarco1154@gmail.com>
2026-06-12 15:22:25 +0200

    add index on orders table

---
+++ database/migrations/2026_06_12_131941_add_index_on_orders_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->index('order_due_at');
+            $table->index(['customer_id', 'order_due_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropIndex('order_due_at');
+            $table->dropIndex(['customer_id', 'order_due_at']);
+        });
+    }
+};
