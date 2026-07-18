564e1e5d6e11db85e75bdaa39cdb237ce9c48bd6
intermaus1154 <amarco1154@gmail.com>
2026-07-18 17:12:39 +0100

    change overlay bg

--- resources/views/livewire/modal/modal-container.blade.php
+++ resources/views/livewire/modal/modal-container.blade.php
@@ -1,7 +1,7 @@
 <div>
     @unless(empty($modalStack))
         {{--backdrop--}}
-        <div class="fixed inset-0 w-full h-full bg-black/30 z-200 justify-center items-start p-4 overflow-y-auto"
+        <div class="fixed inset-0 w-full h-full bg-black/40 z-200 justify-center items-start p-4 overflow-y-auto"
              @mousedown="if($event.target == $el) $dispatch('modal-clear')" @click.self="$dispatch('modal-close')">
             @foreach($modalStack as $index => $modal)
                 @php
@@ -13,7 +13,7 @@
                         @livewire($modal['component'], $modal['data'], $modal['key'])
                     </div>
                 @else
-                    <div class="fixed inset-0 w-full h-full bg-white/60" style="z-index: {{200+$index}}"  @click.self="$dispatch('modal-close')">
+                    <div class="fixed inset-0 w-full h-full bg-black/60" style="z-index: {{200+$index}}"  @click.self="$dispatch('modal-close')">
                         <div class="fixed inset-0 flex justify-center items-start p-4 overflow-y-auto"
                              style="z-index: {{200 + $index}}"  @click.self="$dispatch('modal-close')">
                             @livewire($modal['component'], $modal['data'], $modal['key'])
