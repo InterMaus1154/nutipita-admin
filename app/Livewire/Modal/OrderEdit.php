1d00d273c95fbaa48a0b80b47e57875742bf1d09
intermaus1154 <amarco1154@gmail.com>
2026-07-18 17:18:18 +0100

    change

--- app/Livewire/Modal/OrderEdit.php
+++ app/Livewire/Modal/OrderEdit.php
@@ -42,14 +42,13 @@ class OrderEdit extends Component
 
         try{
             $orderService->updateOrder($this->order, $this->fields, $this->selectedProducts);
-            $this->dispatch('modal-clear')->to(ModalContainer::class);
             $this->dispatch('refresh');
+            $this->dispatch('modal-close')->to(ModalContainer::class);
         }catch (InvalidArgumentException $e){
             $this->addError('products', $e->getMessage());
         }catch (Exception $e){
             $this->addError('general_error', $e->getMessage());
         }
-        $this->dispatch('modal-close')->to(ModalContainer::class);
     }
 
     public function cancel(): void
